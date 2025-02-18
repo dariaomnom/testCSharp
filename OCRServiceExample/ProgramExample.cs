500c0a4f3d8c9e0132e17322ae7306132f9c0224
dariaomnom <dariaomnom@gmail.com>
2025-02-19 02:26:10 +0300

    tests

---
+++ OCRServiceExample/ProgramExample.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using OCRServiceLib;
+
+class ProgramExample
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, Mono!");
+        var myClass = new Class1();
+        string message = myClass.GetMessage();
+        Console.WriteLine(message);
+    }
+}
---
+++ OCRServiceLib/Class1.cs
@@ -0,0 +1,9 @@
+﻿namespace OCRServiceLib {
+	public class Class1
+	{
+	    public string GetMessage()
+	    {
+	        return "Hello from the library!";
+	    }
+	}
+}
